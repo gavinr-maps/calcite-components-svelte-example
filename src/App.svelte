783e48e152ec13a87baf4f2d0b91a0b644b89b12
Gavin Rehkemper <grehkemper@esri.com>
2021-07-22 14:59:38 -0500

    clean svelte template

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,10 @@
 <script>
 	export let name;
-
-	import '@esri/calcite-components/dist/calcite/calcite.css';
-	import { defineCustomElements, setAssetPath } from '@esri/calcite-components/dist/custom-elements';
-
-	setAssetPath(window.location + "build/main.js");
-	defineCustomElements();
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-
-	<p>
-
-		<calcite-icon icon="banana" />
-
-		<calcite-dropdown>
-			<calcite-button slot="dropdown-trigger">Open Dropdown</calcite-button>
-			<calcite-dropdown-group>
-				<calcite-dropdown-item>Relevance</calcite-dropdown-item>
-				<calcite-dropdown-item active>Date modified</calcite-dropdown-item>
-				<calcite-dropdown-item>Title</calcite-dropdown-item>
-			</calcite-dropdown-group>
-		</calcite-dropdown>
-	</p>
 </main>
 
 <style>
